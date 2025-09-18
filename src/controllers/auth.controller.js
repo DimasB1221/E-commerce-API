1fd6ebf6a0749ce109f6969dc8166d3e56031e6d
Dimas Bintang <dimasbintang1818@gmail.com>
2025-09-18 12:13:19 +0700

    add: login register

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,70 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
+
+// Register
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // cek user
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+
+    // hash paswword
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // simpan user baru
+    const user = await User.create({
+      name,
+      password: hashedPassword,
+      email,
+      role: role || "user",
+    });
+
+    // kirim respon ke json
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id, user.role),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Errror :", error: error.message });
+  }
+};
+
+// login
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // cek user
+    const user = await User.find0ne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email" });
+    }
+
+    // cek password
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    // kalau cocok generate token
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id, user.role),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
