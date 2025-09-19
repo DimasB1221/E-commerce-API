c5e596d8c9d0e5c437fd088163f6c05987d05f06
Dimas Bintang <dimasbintang1818@gmail.com>
2025-09-19 08:30:36 +0700

    refactor(user): change _id type back to ObjectId

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import autoIncrement from ("mongoose-sequence")(mongoose);
+import mongoose, { model, Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
-    _id: { type: Number, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["admin", "user"], default: "user" },
-    orders: { type: [mongoose.Schema.Types.ObjectId], ref: "Order" },
-    cart: { type: [mongoose.Schema.Types.ObjectId], ref: "Product" },
+    orders: { type: [Schema.Types.ObjectId], ref: "Order" },
+    cart: { type: [Schema.Types.ObjectId], ref: "Product" },
   },
   { timestamps: true }
 );
-userSchema.plugin(autoIncrement, {inc_field: "_id"});
-export const Users = mongoose.model("User", userSchema);
+// Inisialisasi plugin dengan mongoose
+
+export const Users = model("User", userSchema);
