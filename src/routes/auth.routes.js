02d0adde0c2915f7fad87cb9e9698836569e1b9f
Dimas Bintang <dimasbintang1818@gmail.com>
2025-09-19 08:25:25 +0700

    feat(auth): add authRoutes and integrate into server

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { register, login } from "../controllers/auth.controller.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { admin0nly } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// register user
+router.post("/register", register);
+
+// login user / admin
+router.post("/login", protect, admin0nly, login);
+
+router.get("/me", protect, (req, res) => {
+  res.status(200).json({
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+    role: req.user.role,
+  });
+});
+
+router.get("/admin", admin0nly, (req, res) => {
+  res.status(200).json({ message: "You are an admin" });
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
-import mongoose from "mongoose";
+import connectDB from "./config/db.js";
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import "dotenv/config";
 
 // Connect to MongoDB
-const conn = await mongoose.createConnection(process.env.MONGO_URI).asPromise();
-conn.readyState;
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
