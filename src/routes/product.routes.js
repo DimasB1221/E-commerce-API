cd5463ad97ee49901704bab6d6f914b687727540
Dimas Bintang <dimasbintang1818@gmail.com>
2025-09-21 19:37:49 +0700

    feat(product): add Product routes

---
+++ src/routes/product.routes.js
@@ -0,0 +1,27 @@
+import { getAllProducts } from "../controllers/product.controller.js";
+import { getProductById } from "../controllers/product.controller.js";
+import { createProduct } from "../controllers/product.controller.js";
+import { updateProduct } from "../controllers/product.controller.js";
+import { deleteProduct } from "../controllers/product.controller.js";
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import { admin0nly } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// get all products
+router.get("/", protect, getAllProducts);
+
+// get product by id
+router.get("/:id", protect, getProductById);
+
+// create product - admin only
+router.post("/", admin0nly, createProduct);
+
+// update product - admin only
+router.put("/:id", admin0nly, updateProduct);
+
+// delete product - admin only
+router.delete("/:id", admin0nly, deleteProduct);
+
+export default router;
