2c690737c349f47c4cee287b174d711dde2f6035
Dimas Bintang <dimasbintang1818@gmail.com>
2025-09-21 19:34:15 +0700

    chore(server): integrate product routes into server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import connectDB from "./config/db.js";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import productRoutes from "./routes/product.routes.js";
 import "dotenv/config";
 
 // Connect to MongoDB
@@ -9,7 +10,11 @@ const app = express();
 
 app.use(express.json());
 
+// auth routes
 app.use("/api/auth", authRoutes);
+
+// product routes
+app.use("/api/products", productRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
