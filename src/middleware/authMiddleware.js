25c2d8d12af3bf70e1aeec1b849c3240ce66d58c
Dimas Bintang <dimasbintang1818@gmail.com>
2025-10-05 09:39:49 +0700

    fix(auth): update protect middleware to return 401 instead of 404 when token is missing

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 export const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(404).json({ message: "Token not found" });
+    return res.status(401).json({ message: "Token not found" });
   }
   const token = authHeader.split(" ")[1];
   if (!token) {
