7a6744b2eeef9c9905896c01d966632f31905a11
Dimas Bintang <dimasbintang1818@gmail.com>
2025-10-14 16:42:35 +0700

    minor change

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     role: { type: String, enum: ["admin", "user"], default: "user" },
     orders: { type: [Schema.Types.ObjectId], ref: "Order" },
     cart: { type: [Schema.Types.ObjectId], ref: "Product" },
